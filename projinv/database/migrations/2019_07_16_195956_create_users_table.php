1ff809de5cf9cda6939a90943ed6c66deaf8952b
gff1055 <guilhermeferreirafb@gmail.com>
2019-07-22 17:51:24 -0300

    Criacao da tabela users; definicao da tabela usersocial

--- projinv/database/migrations/2019_07_16_195956_create_users_table.php
+++ projinv/database/migrations/2019_07_16_195956_create_users_table.php
@@ -13,6 +13,8 @@ class CreateUsersTable extends Migration
 	 *
 	 * @return void
 	 */
+
+	/* Onde as tabelas sao criadas via migrate*/
 	public function up()
 	{
 		Schema::create('users', function(Blueprint $table) {
@@ -32,10 +34,13 @@ class CreateUsersTable extends Migration
 
 			// campos de permissao
 			$table->string('status')->default('active'); //definindo valor default do campo
-			$table->string('permission')->default('app.user'); //definindo valor padrao app.user
+			$table->string('permission')->default('app.user'); //definindo padrao de permissao (app.user)
+
+			$table->rememberToken(); // cria campo para redefinicao de senha
+            $table->timestamps();	// cria dois campos (created_at e updated_at) (criacao e atualizacao de regitros)
+            $table->softDeletes(); // apaga o registro (registro não é apagado no banco, mas para a aplicação ele é setado como excluido)
 
 
-            $table->timestamps();
 		});
 	}
 
@@ -44,8 +49,16 @@ class CreateUsersTable extends Migration
 	 *
 	 * @return void
 	 */
+
+	/* Onde as tabelas sao removidas */
 	public function down()
 	{
+		/* Indica que sera feita uma alteracao na tabela
+		Será usado na hora de inserir os relacionamentos */
+		Schema::table('users', function(Blueprint $table) {
+
+		});
+
 		Schema::drop('users');
 	}
 }
