b9fbd5b3d47ade4dca1160d10a42b53501f586d8
gff1055 <guilhermeferreirafb@gmail.com>
2020-01-28 14:12:53 -0300

    Attach(1) [dsnv]

--- projinv/resources/views/groups/show.blade.php
+++ projinv/resources/views/groups/show.blade.php
@@ -2,5 +2,28 @@
 @extends('templates.master')
 
 @section('conteudo-view')
-OI
+
+<header>
+	<h1>Nome do grupo: {{ $group->name }}</h1>
+	<h2>Instituição: {{ $group->institution->name }}</h2>
+	<h2>Nome do responsavel: {{ $group->owner->name }}</h2>
+</header>
+
+{!! Form::open(	[
+					'method' => 'post',
+					'class' => 'form-padrao'
+]) !!}
+
+	@include('templates.formulario.select',	[
+												'label' => "Usuário",
+												'select' => 'user_id',
+												'data' => $user_list,
+												'attributes' => ['placeholder' => "Usuário"]
+											]
+	)
+
+	@include('templates.formulario.submit',	['input' => 'Relacionar ao grupo' . $group->name])
+	
+{!! Form::close() !!}
+
 @endsection
